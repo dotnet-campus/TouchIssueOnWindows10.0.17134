8bc14ea24d37ad8eed567912fe110060a01ade30
walterlv <xblero@gmail.com>
2018-05-17 10:40:25 +0800

    Add simple reproduction code for touch issue.

---
+++ TheAppToInvestigateWhatHappened/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TheAppToInvestigateWhatHappened
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnStylusDown(object sender, StylusDownEventArgs e)
+        {
+            // Set a breakpoint here.
+        }
+    }
+}
